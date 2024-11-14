6e75995206f487dc28c2f1f7d7407dc28f2931d5
theMargarita <margotvoistinova@hotmail.se>
2024-11-14 21:28:50 +0100

    not finished but progress

--- Restaurant.cs
+++ Restaurant.cs
@@ -1,45 +1,56 @@
-﻿namespace GenericCollections
-{
+﻿using System.Collections.Generic;
 
+namespace GenericCollections
+{
     internal class Restaurant
     {
+        //just wanted to see if there would be any different
         private List<MenuItem> _menuItem;
-        private Queue<Order> _order;
+        private Queue<Order> _order = new Queue<Order>();
 
         public Restaurant()
         {
             _menuItem = new List<MenuItem>();
             _order = new Queue<Order>();
         }
+
+        //adds food to the menu
         public void AddToMenu(MenuItem menuItem)
         {
             _menuItem.Add(menuItem);
+            Console.WriteLine($"{menuItem} is now added to the menu.");
         }
 
+        //method explains it
         public void ShowMenu()
         {
+
+            Console.WriteLine("----------------------");
             Console.WriteLine("Menu: ");
             //menu is the var varible in _menuItem
             foreach (var menu in _menuItem)
             {
                 Console.WriteLine(menu);
             }
-            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------\n");
         }
 
+        //creates and adds the order
         public void CreateOrder(Order order)
         {
             _order.Enqueue(order);
             Console.WriteLine($"Order: {order}");
+            //Console.WriteLine("----------------------");
         }
 
-        //dequeing part
+        //dequeing part - handles the order
         public void HandleOrder()
         {
             if (_order.Count > 0)
             {
-                _order.Dequeue();
-                Console.WriteLine($"Handleing order: {_order}");
+                ToString();
+                Console.WriteLine($"Handleing order: {_order.Dequeue()}");
+                Console.WriteLine("----------------------");
             }
             else
             {
@@ -47,19 +58,32 @@
             }
         }
 
+        //methodname explains it
+
         public void ShowOrder()
         {
-
+            //shows every order in que/line (samesame)
+            foreach(var orders in _order)
+            {
+                Console.WriteLine($"There are {orders} orders in line.");
+                //Console.WriteLine("----------------------");
+            }
         }
 
+        //method name explains it
         public void ShowNextOrer()
         {
-            _order.Peek();
+            //peek peeks into the next order
+            Console.WriteLine($"Next order in line: {_order.Peek()}");
         }
 
+        //writes out the amount the orders in que
         public void ShowOrderCount()
         {
-
+            foreach(var showOrder in _order)
+            {
+                Console.WriteLine(showOrder);
+            }
         }
     }
 }
